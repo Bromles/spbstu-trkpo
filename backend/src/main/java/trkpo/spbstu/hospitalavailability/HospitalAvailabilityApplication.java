6337040031de30dc48fe020994b5811141f2f807
bromles <all.bromles@gmail.com>
2023-09-23 15:52:14 +0300

    Done initial setup

---
+++ backend/src/main/java/trkpo/spbstu/hospitalavailability/HospitalAvailabilityApplication.java
@@ -0,0 +1,11 @@
+package trkpo.spbstu.hospitalavailability;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalAvailabilityApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalAvailabilityApplication.class, args);
+	}
+}
---
+++ backend/src/test/java/trkpo/spbstu/hospitalavailability/HospitalAvailabilityApplicationTests.java
@@ -0,0 +1,13 @@
+package trkpo.spbstu.hospitalavailability;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HospitalAvailabilityApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
