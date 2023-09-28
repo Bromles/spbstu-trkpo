6a53571cabd2114750d5c89aa6463db2ea8d6dd4
Ekaterina Borisova <kate_boriso2002@mail.ru>
2023-09-28 15:48:57 +0300

    fix after review iteration 1

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react';
 import reactLogo from '@/assets/react.svg';
 import viteLogo from '/vite.svg';
-import styles from '@/styles/partials/App.module.css';
+import styles from '@/App.module.css';
 
 export const App = () => {
   const [count, setCount] = useState(0);
