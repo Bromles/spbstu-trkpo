f7cf4c5b219b5651edb0a97e2b473a044d7a5c5b
Ekaterina Borisova <kate_boriso2002@mail.ru>
2023-09-29 13:02:20 +0300

    delete unused lib

--- frontend/src/utils/Providers.tsx
+++ frontend/src/utils/Providers.tsx
@@ -1,14 +1,9 @@
-import i18n from "@/utils/i18n.ts";
-import {createContext} from "react";
-
-const I18nContext = createContext<typeof i18n>({} as typeof i18n);
+import React from "react";
 
 export const Providers = ({children}: { children: React.ReactNode }) => {
     return (
         <>
-            <I18nContext.Provider value={i18n}>
-                {children}
-            </I18nContext.Provider>
+            {children}
         </>
     );
 };
