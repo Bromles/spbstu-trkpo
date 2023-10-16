198fa52f67d26121559d775a26d874bef5d67571
bromles <all.bromles@gmail.com>
2023-10-16 06:08:04 +0300

    fix import paths

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,7 @@
 import { Outlet } from "react-router-dom";
-import { AuthGuard } from "./components/AuthGuard/AuthGuard";
-import { NavBar } from "./components/Navbar/Navbar";
-import { NavigationLoading } from "./components/NavigationLoading/NavigationLoading";
+import { AuthGuard } from "@/components/AuthGuard/AuthGuard";
+import { NavBar } from "@/components/Navbar/Navbar";
+import { NavigationLoading } from "@/components/NavigationLoading/NavigationLoading";
 
 export const App = () => {
   return (
