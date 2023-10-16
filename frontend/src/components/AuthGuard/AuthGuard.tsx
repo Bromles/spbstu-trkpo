c2b976a283db59ea44bac8b6c8c873e746b7c533
bromles <all.bromles@gmail.com>
2023-10-16 03:20:19 +0300

    setup oidc with keycloak, add component stubs

---
+++ frontend/src/components/AuthGuard/AuthGuard.tsx
@@ -0,0 +1,27 @@
+import { useAuth } from "react-oidc-context";
+import React from "react";
+
+export const AuthGuard = ({ children }: { children: React.ReactNode }) => {
+  const auth = useAuth();
+
+  switch (auth.activeNavigator) {
+    case "signinSilent":
+      return <div>Signing you in ...</div>;
+    case "signoutRedirect":
+      return <div>Signing you out ...</div>;
+  }
+
+  if (auth.isLoading) {
+    return <div>Auth is loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Oops... Auth error {auth.error.message}</div>;
+  }
+
+  if (!auth.isAuthenticated) {
+    return <>You are not logged in</>;
+  } else {
+    return <>{children}</>;
+  }
+};
--- frontend/src/utils/Providers.tsx
+++ frontend/src/utils/Providers.tsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { AuthProvider } from "react-oidc-context";
+import { oidcConfig } from "./oidcConfig";
 
-export const Providers = ({children}: { children: React.ReactNode }) => {
-    return (
-        <>
-            {children}
-        </>
-    );
+export const Providers = ({ children }: { children: React.ReactNode }) => {
+  return (
+    <>
+      <AuthProvider {...oidcConfig}>
+          {children}
+      </AuthProvider>
+    </>
+  );
 };
