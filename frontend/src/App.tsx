28e804814dab95a4c56feecb9b55504ff2742a81
bromles <all.bromles@gmail.com>
2023-10-18 10:45:21 +0300

    fix main container styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,16 +2,19 @@ import { Outlet } from "react-router-dom";
 import { AuthGuard } from "@/components/AuthGuard/AuthGuard";
 import { NavBar } from "@/components/Navbar/Navbar";
 import { NavigationLoading } from "@/components/NavigationLoading/NavigationLoading";
+import styles from "./App.module.css";
 
 export const App = () => {
   return (
     <>
       <NavBar />
-      <NavigationLoading>
-        <AuthGuard>
-          <Outlet />
-        </AuthGuard>
-      </NavigationLoading>
+      <div className={styles.mainContainer}>
+        <NavigationLoading>
+          <AuthGuard>
+            <Outlet />
+          </AuthGuard>
+        </NavigationLoading>
+      </div>
     </>
   );
 };
