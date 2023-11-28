c29e9a0e7860483a29485bc4af85cde96ac863ec
darya.dyurdeva <darya.dyurdeva@vk.team>
2023-11-28 19:21:37 +0300

    small fix

--- backend/src/main/java/trkpo/spbstu/hospitalavailability/repository/TrackingRepository.java
+++ backend/src/main/java/trkpo/spbstu/hospitalavailability/repository/TrackingRepository.java
@@ -1,12 +1,11 @@
 package trkpo.spbstu.hospitalavailability.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.query.Param;
 import trkpo.spbstu.hospitalavailability.entity.Tracking;
 import java.util.List;
 
 public interface TrackingRepository extends JpaRepository<Tracking, Long> {
-    List<Tracking> findByIsFinishedFalse();
 
-    long removeById(@Param("id") Long id);
+    List<Tracking> findByIsFinishedFalse();
+    long removeById(Long id);
 }
