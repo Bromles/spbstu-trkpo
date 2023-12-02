64d1a9fe8acdb67fef336b2c07420cd1e57521ac
kate-bor <kate_boriso2002@mail.ru>
2023-12-02 16:49:35 +0300

    delete unused annotations

--- backend/src/main/java/trkpo/spbstu/hospitalavailability/repository/HospitalRepository.java
+++ backend/src/main/java/trkpo/spbstu/hospitalavailability/repository/HospitalRepository.java
@@ -3,12 +3,8 @@ package trkpo.spbstu.hospitalavailability.repository;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
-import org.springframework.stereotype.Repository;
 import trkpo.spbstu.hospitalavailability.entity.Hospital;
 
-@Repository
 public interface HospitalRepository extends JpaRepository<Hospital, Long> {
-    @Query(value = "select h from Hospital h where h.gorzdravId = ?1")
     Optional<Hospital> findByGorzdravId(long gorzdravId);
 }
