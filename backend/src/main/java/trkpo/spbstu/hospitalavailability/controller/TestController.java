b277d5f7d9e9cb68420e9517f27b55cfc8d7b04c
kate-bor <kate_boriso2002@mail.ru>
2023-12-02 20:25:43 +0300

    small changes

--- backend/src/main/java/trkpo/spbstu/hospitalavailability/controller/TestController.java
+++ backend/src/main/java/trkpo/spbstu/hospitalavailability/controller/TestController.java
@@ -1,7 +1,6 @@
 package trkpo.spbstu.hospitalavailability.controller;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,8 +15,7 @@ public class TestController {
     private final NotificationMailSender sender;
 
     @PostMapping("/sendMessage")
-    public ResponseEntity<String> sendMail() {
+    public void sendMail() {
         sender.sendMessage("spbstu.trkpo@yandex.ru", "Я работаю!");
-        return ResponseEntity.ok().build();
     }
 }
--- backend/src/main/java/trkpo/spbstu/hospitalavailability/service/NotificationMailSender.java
+++ backend/src/main/java/trkpo/spbstu/hospitalavailability/service/NotificationMailSender.java
@@ -3,7 +3,6 @@ package trkpo.spbstu.hospitalavailability.service;
 import javax.validation.constraints.Email;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.ResponseEntity;
 import org.springframework.mail.MailException;
 import org.springframework.mail.MailSender;
@@ -19,11 +18,10 @@ import trkpo.spbstu.hospitalavailability.exception.BackendUnavailableException;
 public class NotificationMailSender {
     private final MailSender mailSender;
 
-//    @Value("${spring.mail.username}")
     private final String senderEmail = "spbstu.trkpo@yandex.ru";
 
-    @Retryable(value = MailException.class, maxAttempts = 3, backoff = @Backoff(delay = 100))
-    public ResponseEntity<String> sendMessage(@Email String userMail, String message) throws MailException {
+    @Retryable(value = MailException.class, maxAttempts = 2, backoff = @Backoff(delay = 100))
+    public void sendMessage(@Email String userMail, String message) throws MailException {
         final SimpleMailMessage simpleMail = new SimpleMailMessage();
         simpleMail.setFrom(senderEmail);
         simpleMail.setTo(userMail);
@@ -31,11 +29,10 @@ public class NotificationMailSender {
         simpleMail.setText(message);
 
         this.mailSender.send(simpleMail);
-        return ResponseEntity.ok().build();
     }
 
     @Recover
-    public ResponseEntity<String> recoverSendMessage(@Email String userMail, String message, MailException e) {
+    public void recoverSendMessage(@Email String userMail, String message, MailException e) {
         throw new BackendUnavailableException("Cannot send email to user: " + userMail);
     }
 
