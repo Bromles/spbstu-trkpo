7b9e7e63e8d802e4e0183fdf646b3d78373dc1c2
darya.dyurdeva <darya.dyurdeva@vk.team>
2023-12-03 20:25:28 +0300

    fix

--- backend/src/main/java/trkpo/spbstu/hospitalavailability/repository/TrackingRepository.java
+++ backend/src/main/java/trkpo/spbstu/hospitalavailability/repository/TrackingRepository.java
@@ -3,6 +3,7 @@ package trkpo.spbstu.hospitalavailability.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.lang.NonNull;
 import trkpo.spbstu.hospitalavailability.entity.Tracking;
 
@@ -16,13 +17,12 @@ public interface TrackingRepository extends JpaRepository<Tracking, Long> {
     List<Tracking> findByIsFinishedFalseAndClientId(Long id);
 
     @NonNull
-    @Modifying
     @SuppressWarnings({"overrides", "unchecked"})
     @Query(value = "insert into tracking(date, direction_id, doctor_id, is_finished, client_id, hospital_id) values " +
-            "(:track.date, :track.directionId, :track.doctorId, :track.finished, :track.client, :track.hospital)" +
+            "(:track.date, :track.direction_id, :track.doctorId, :track.is_finished, :track.client.id, :track.hospital.id) " +
             "on conflict(direction_id, doctor_id, client_id, hospital_id) do update SET " +
             "date = excluded.date, is_finished = excluded.is_finished returning *", nativeQuery = true)
-    Tracking save(@NonNull Tracking track);
+    Tracking save(@NonNull @Param("track") Tracking track);
 
     @Modifying
     @Query("update Tracking t set t.isFinished = :finished where t.id = :id")
