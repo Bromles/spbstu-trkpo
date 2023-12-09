f2e4b4c63ff9ea09fdeb30cd2cb301a95f3a8171
Bromles <all.bromles@gmail.com>
2023-12-09 10:46:12 +0300

    update placemark display

--- frontend/src/components/HospitalMap/HospitalMap.tsx
+++ frontend/src/components/HospitalMap/HospitalMap.tsx
@@ -1,27 +1,60 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import { Map, Placemark, ZoomControl } from "@pbe/react-yandex-maps";
 import { MouseEvent, useCallback, useEffect, useState } from "react";
 import { createPortal } from "react-dom";
 import styles from "./HospitalMap.module.css";
 import "./HospitalMap.css";
-import { useAuth } from "react-oidc-context";
 
+import {
+  useClientToken,
+  useGlobalStore,
+  useSelectionStore,
+} from "@/utils/hooks";
+import { observer } from "mobx-react-lite";
+import { Hospital } from "@/utils/types";
+import { fetchHospitals } from "@/pages/Home/HomeApi";
 import { getBackendUrl } from "@/utils/apiUtils";
-import { Hospital, Direction, Doctor } from "@/utils/types";
+import { autorun } from "mobx";
 
-export const HospitalMap = () => {
+export const HospitalMap = observer(() => {
   const [activePortal, setActivePortal] = useState(false);
-  const [selectedPlacemarkId, setSelectedPlacemarkId] = useState<number | null>(
-    null
+  const [filteredHospitals, setFilteredHospitals] = useState<Hospital[]>([]);
+  const clientToken = useClientToken();
+  const globalStore = useGlobalStore();
+  const selectionStore = useSelectionStore();
+
+  useEffect(() => {
+    const backendUrl = getBackendUrl();
+
+    const fetchData = async () => {
+      globalStore.hospitals = await fetchHospitals(backendUrl, clientToken);
+    };
+
+    fetchData();
+  }, [clientToken]);
+
+  useEffect(
+    () =>
+      autorun(() => {
+        const backendUrl = getBackendUrl();
+
+        const fetchData = async () => {
+          const data = await fetchHospitals(backendUrl, clientToken);
+          const filtered = data.filter(
+            (hospital: Hospital) =>
+              selectionStore.selectedDistrictId === hospital.districtId
+          );
+          setFilteredHospitals(filtered);
+        };
+
+        if (selectionStore.selectedDistrictId !== -1) {
+          fetchData();
+        } else {
+          setFilteredHospitals(globalStore.hospitals);
+        }
+      }),
+    [clientToken]
   );
-  const [isEventSet, setIsEventSet] = useState(false);
-
-  const getHospital = () => {
-    if (selectedPlacemarkId && hospitals) {
-      return hospitals[selectedPlacemarkId];
-    }
-
-    return null;
-  };
 
   return (
     <div className={styles.map_container}>
@@ -41,7 +74,7 @@ export const HospitalMap = () => {
           className={styles.map}
         >
           <ZoomControl />
-          {hospitals?.map((hospital) => (
+          {filteredHospitals.map((hospital) => (
             <Placemark
               key={hospital.id}
               defaultGeometry={[hospital.latitude, hospital.longitude]}
@@ -52,24 +85,20 @@ export const HospitalMap = () => {
               onClick={(e: MouseEvent<HTMLElement>) => {
                 setTimeout(() => {
                   console.log(`react click target: ${e.target}`);
-                  const closeButton = [
+                  const selected = [
                     ...document.querySelectorAll(
                       'ymaps[class$="-balloon__close"]'
                     ),
-                  ].forEach((el) => console.log(`selected target: ${el}`));
-                  if (
-                    closeButton !== null &&
-                    closeButton !== undefined &&
-                    !isEventSet
-                  ) {
-                    // closeButton.addEventListener('click', () => {
-                    //   setSelectedPlacemarkId(null);
-                    //   setActivePortal(false);
-                    //   setIsEventSet(true);
-                    // });
+                  ];
+                  selected.forEach((el) => console.log(el));
+                  const closeButton = selected[0];
+                  if (closeButton !== null && closeButton !== undefined) {
+                    closeButton.addEventListener("click", () => {
+                      setActivePortal(false);
+                    });
                   }
 
-                  setSelectedPlacemarkId(hospital.id);
+                  selectionStore.selectedHospitalId = hospital.id;
                   setActivePortal(true);
                 }, 0);
               }}
@@ -77,118 +106,63 @@ export const HospitalMap = () => {
           ))}
         </Map>
         {activePortal && (
-          <BalloonPortal elementId={`hospital-balloon-${selectedPlacemarkId}`}>
-            <BalloonContent hospital={getHospital()} />
+          <BalloonPortal
+            elementId={`hospital-balloon-${selectionStore.selectedHospitalId}`}
+          >
+            <BalloonContent hospital={hospital!} />
           </BalloonPortal>
         )}
       </div>
     </div>
   );
-};
+});
+HospitalMap.displayName = "HospitalMap";
 
-const BalloonContent = ({
-  hospital,
-  direction,
-  doctor,
-}: {
-  hospital: Hospital | null;
-  direction: Direction | null;
-  doctor: Doctor | null;
-}) => {
-  const auth = useAuth();
+const BalloonContent = observer(({ hospital }: { hospital: Hospital }) => {
+  const selectionStore = useSelectionStore();
 
-  const startTrackingHandler = useCallback(() => {
-    const backendURL = getBackendUrl();
-
-    const sendData = async () => {
-      let body;
-      const errorSection = document.getElementById("errorSection");
-      const successSection = document.getElementById("successSection");
-      if (selectedDirectionId === -1 || selectedHospitalId === -1) {
-        if (errorSection !== null) {
-          errorSection.textContent = "Некорректные данные";
-        }
-        if (successSection !== null) {
-          successSection.textContent = null;
-        }
-        throw new Error("Некорректные данные");
-      } else if (errorSection !== null) {
-        errorSection.textContent = null;
-      }
-      if (selectedDoctorId !== -1) {
-        body = JSON.stringify({
-          hospitalId: hospital.id,
-          directionId: selectedDirectionId,
-          doctorId: selectedDoctorId,
-        });
-      } else {
-        body = JSON.stringify({
-          hospitalId: selectedHospitalId,
-          directionId: selectedDirectionId,
-        });
-      }
-      try {
-        const response = await fetch(`${backendURL}/v1/tracking`, {
-          method: "POST",
-          headers: {
-            "Content-Type": "application/json",
-            Authorization: `Bearer ${auth.user?.access_token}`,
-          },
-          body: body,
-        });
-
-        if (response.ok) {
-          if (successSection !== null) {
-            successSection.textContent = "Успех!";
-          }
-          console.log("Отслеживание успешно начато!");
-        } else {
-          if (errorSection !== null) {
-            errorSection.textContent = "Ошибка при отправке данных.";
-          }
-          console.error("Ошибка при отправке данных.");
-        }
-      } catch (error) {
-        console.error("Ошибка во время запроса:", error);
-      }
-    };
-
-    sendData();
-  }, [auth.user?.access_token]);
+  const selectionHandler = useCallback(() => {
+    selectionStore.selectedHospitalId = hospital!.id;
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [hospital]);
 
   return (
     <>
       <div className={styles.modal_container}>
-        {hospital?.fullName}
-        <button onClick={startTrackingHandler}>Начать отслеживание</button>
+        {hospital.fullName}
+        <button onClick={selectionHandler}>Выбрать</button>
       </div>
     </>
   );
-};
+});
+BalloonContent.displayName = "BalloonContent";
 
-const BalloonPortal = ({
-  children,
-  elementId,
-}: {
-  children: React.ReactNode;
-  elementId: string;
-}) => {
-  const mount = document.getElementById(elementId);
-  const el = document.createElement("div");
+const BalloonPortal = observer(
+  ({
+    children,
+    elementId,
+  }: {
+    children: React.ReactNode;
+    elementId: string;
+  }) => {
+    const mount = document.getElementById(elementId);
+    const el = document.createElement("div");
 
-  useEffect(() => {
-    if (mount) {
-      mount.appendChild(el);
-    }
-
-    return () => {
+    useEffect(() => {
       if (mount) {
-        mount.removeChild(el);
+        mount.appendChild(el);
       }
-    };
-  }, [mount, el]);
 
-  if (!mount) return null;
+      return () => {
+        if (mount) {
+          mount.removeChild(el);
+        }
+      };
+    }, [mount, el]);
 
-  return createPortal(children, el);
-};
+    if (!mount) return null;
+
+    return createPortal(children, el);
+  }
+);
+BalloonPortal.displayName = "BalloonPortal";
