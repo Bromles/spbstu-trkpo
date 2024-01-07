c6e40b3dbb86a6af9a0d426b34c12da838b969ad
Ekaterina Borisova <90840804+KateBor@users.noreply.github.com>
2024-01-07 21:43:35 +0300

    Update frontend/src/pages/NotFound/NotFound.tsx
    Co-authored-by: Aleksandr Ushakov <34621477+Bromles@users.noreply.github.com>

--- frontend/src/pages/NotFound/NotFound.tsx
+++ frontend/src/pages/NotFound/NotFound.tsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 export const NotFound = () => {
   return (
     <>
-      <p>Not Found</p>
+      <p>Не найдено</p>
       <p>
         <Link to="/">На главную</Link>
       </p>
