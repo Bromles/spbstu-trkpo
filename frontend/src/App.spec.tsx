51559ff715620b850b33eeb95826e9499d279622
bromles <all.bromles@gmail.com>
2024-02-19 23:59:27 +0300

    Add app tests

---
+++ frontend/src/App.spec.tsx
@@ -0,0 +1,30 @@
+import { render, screen } from "@testing-library/react";
+import { Mock, afterEach, describe, expect, vi } from "vitest";
+import { App } from "./App";
+import { useAuth } from "react-oidc-context";
+import { useNavigation } from "react-router-dom";
+
+vi.mock("react-oidc-context", () => ({
+  useAuth: vi.fn(),
+}));
+
+vi.mock("react-router-dom", () => ({
+  useNavigation: vi.fn(),
+  Outlet: vi.fn()
+}));
+
+describe("App", () => {
+  afterEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it("should render", () => {
+    const auth = {};
+    const navigation = {};
+
+    (useNavigation as Mock).mockReturnValue(navigation);
+    (useAuth as Mock).mockReturnValue(auth);
+
+    render(<App />);
+  });
+});
