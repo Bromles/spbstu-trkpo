6108ed1514a890bd142542f1145da478ff6f167d
darya.dyurdeva <darya.dyurdeva@vk.team>
2024-03-16 15:43:11 +0300

    rename

--- backend/src/test/java/trkpo/spbstu/hospitalavailability/e2e/tests/E2eLoginTest.java
+++ backend/src/test/java/trkpo/spbstu/hospitalavailability/e2e/tests/E2eLoginTest.java
@@ -3,10 +3,10 @@ package trkpo.spbstu.hospitalavailability.e2e.tests;
 import org.junit.jupiter.api.Test;
 import trkpo.spbstu.hospitalavailability.e2e.E2eTestBase;
 
-public class E2eTestLogin extends E2eTestBase {
+public class E2eLoginTest extends E2eTestBase {
 
     @Test
-    public void E2eTestGoToLoginPage() {
+    public void testGoToLoginPage() {
         goToLoginPage();
     }
 }
