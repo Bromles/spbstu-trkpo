d6119a8986ed3ef27344e709829a53d1cd0aaf16
Bromles <all.bromles@gmail.com>
2024-03-23 11:22:36 +0300

    fix e2e test on windows

--- backend/src/test/java/trkpo/spbstu/hospitalavailability/e2e/BaseTest.java
+++ backend/src/test/java/trkpo/spbstu/hospitalavailability/e2e/BaseTest.java
@@ -1,8 +1,8 @@
 package trkpo.spbstu.hospitalavailability.e2e;
 
+import com.codeborne.selenide.Configuration;
 import org.junit.BeforeClass;
 import org.junit.jupiter.api.TestInstance;
-import com.codeborne.selenide.Configuration;
 import trkpo.spbstu.hospitalavailability.e2e.pages.UnauthPage;
 
 import static com.codeborne.selenide.Selenide.open;
@@ -14,7 +14,13 @@ public class BaseTest {
 
     @BeforeClass
     public static void startDriver() {
-        System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver");
+        if (System.getProperty("os.name", "").contains("Windows")) {
+            System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver.exe");
+            Configuration.browserBinary = "C:/Program Files/Mozilla Firefox/firefox.exe";
+        } else {
+            System.setProperty("webdriver.gecko.driver", "./src/test/resources/geckodriver");
+        }
+
         Configuration.browser = "firefox";
         Configuration.baseUrl = "http://localhost:4200";
         Configuration.browserSize = "1920x1080";
