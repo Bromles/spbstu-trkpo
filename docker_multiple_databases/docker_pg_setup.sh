49b008ccc5ec64cb1af22da1843e618affb23e51
Bromles <all.bromles@gmail.com>
2024-03-25 01:33:54 +0300

    create backend db dump

--- docker_multiple_databases/docker_pg_setup.sh
+++ docker_multiple_databases/docker_pg_setup.sh
@@ -7,5 +7,6 @@ set -x
 echo "Start database restoration"
 
 pg_restore -p 5432 -U "$POSTGRES_USER" -v -d keycloak_db -Fc /docker-entrypoint-initdb.d/backup
+pg_restore -p 5432 -U "$POSTGRES_USER" -v -d backend_db -Fc /docker-entrypoint-initdb.d/backup-backend
 
 set +x
